e2cac19e3e9a6a03d30f8ab332e62218fa24a680
kinseeen <kine.jakobsen@hotmail.no>
2025-09-01 14:57:41 +0200

    install dependencies

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./AppTest";
+import "./index.css"; // Tailwind or other styles
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
