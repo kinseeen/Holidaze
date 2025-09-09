a3dae01ca8eff4c755266177626c1cd7d22d49b8
kinseeen <kine.jakobsen@hotmail.no>
2025-09-09 11:06:41 +0200

    Update GuestFilter.jsx

--- src/components/GuestFilter.jsx
+++ src/components/GuestFilter.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import FilterBoxMin from "./FilterBoxMin";
 import FilterBoxMax from "./FilterBoxMax";
 
-function GuestFilter() {
+function GuestFilter({ minGuest, setMinGuest, maxGuest, setMaxGuest }) {
   return (
     <div className="">
       <h2 className="text-lg font-normal">Guest capacity</h2>
       <div className="flex items-center gap-2">
-        <FilterBoxMin />
+        <FilterBoxMin value={minGuest} onChange={setMinGuest} />
         <span>-</span>
-        <FilterBoxMax />
+        <FilterBoxMax value={maxGuest} onChange={setMaxGuest} />
       </div>
     </div>
   );
